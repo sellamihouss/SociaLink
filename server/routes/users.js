a6e236835456098660c62753e8d86dc2f2eef684
SELLAMI Houssem <sellami.houssem1@gmail.com>
2023-07-25 16:53:37 +0100

    user routes

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,5 +7,13 @@ import {
 import {verifyToken} from "../middleware/auth.js"
 
 
-//Read
 const router =express.Router()
+
+//Read
+router.get('/:id', verifyToken,getUser) 
+router.get('/id/friends', verifyToken,getUserFriends)
+
+//Update
+router.patch("/:id/friendId",verifyToken,addRemoveFriend)
+
+export default router
